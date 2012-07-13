1a2bf2c081d066506f18383558fc5b49d74c49fb
Nikez <nikez141@gmail.com>
2012-07-13 17:46:57 +0200

    ICS-INIT

---
+++ src/com/android/launcher2/preference/Preferences.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2008 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.launcher2.preference;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.util.Log;
+
+import com.android.launcher.R;
+
+public class Preferences extends PreferenceActivity implements 
+        OnPreferenceChangeListener {
+
+    private static final String TAG = "Launcher.Preferences";
+
+    private static final String SEARCHBAR_PREF = "ui_homescreen_general_search";
+    private static final String SCROLLING_PREF = "ui_homescreen_scrolling_scroll_wallpaper";
+    
+    private SharedPreferences mPrefs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+
+        mPrefs = getSharedPreferences(PreferencesProvider
+                .PREFERENCES_KEY, Context.MODE_PRIVATE);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen
+            , Preference preference) {
+        if (preference.getKey().equals(SEARCHBAR_PREF)) {
+            setForReset();
+        } else if (preference.getKey().equals(SCROLLING_PREF)) {
+            setForReset();
+        }
+        return false;
+    }
+
+    private void setForReset() {
+        SharedPreferences.Editor editor = mPrefs.edit();
+        editor.putBoolean(PreferencesProvider.PREFERENCES_CHANGED, true);
+        editor.commit();
+    }
+
+}
---
+++ src/com/android/launcher2/preference/PreferencesProvider.java
@@ -0,0 +1,39 @@
+package com.android.launcher2.preference;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public final class PreferencesProvider {
+    public static final String PREFERENCES_KEY = "com.android.launcher_preferences";
+
+    public static final String PREFERENCES_CHANGED = "preferences_changed";
+
+    public static class Interface {
+        public static class Homescreen {
+            public static boolean getShowSearchBar(Context context) {
+                final SharedPreferences preferences = context.getSharedPreferences(PREFERENCES_KEY, 0);
+                return preferences.getBoolean("ui_homescreen_general_search", false);
+            }
+                public static boolean getScrollWallpaper(Context context) {
+                    final SharedPreferences preferences = context.getSharedPreferences(PREFERENCES_KEY, 0);
+                    return preferences.getBoolean("ui_homescreen_scrolling_scroll_wallpaper", true);
+                }
+        }
+
+        public static class Drawer {
+
+        }
+
+        public static class Dock {
+
+        }
+
+        public static class Icons {
+
+        }
+    }
+
+    public static class General {
+
+    }
+}
